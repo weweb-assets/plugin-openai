24ddcf7ee2b3735cf724ee8fc51a5e4eb09f6cbb
Dams-d <damien.duro8@gmail.com>
2023-03-21 12:22:04 +0100

    Fix

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -1,7 +1,7 @@
 /* wwEditor:start */
 import './components/Configuration/SettingsEdit.vue';
 import './components/Configuration/SettingsSummary.vue';
-// import './components/Functions/CreateCompletion.vue';
+import './components/Functions/CreateCompletion.vue';
 import './components/Functions/CreateChatCompletion.vue';
 import './components/Functions/CreateImage.vue';
 import './components/Functions/CreateEdit.vue';
