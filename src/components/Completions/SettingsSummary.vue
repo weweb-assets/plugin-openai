578f114877d5b9850c9035da51260c20abdd78e0
Dams-d <damien.duro8@gmail.com>
2023-03-24 14:41:24 +0100

    Fix

--- src/components/Completions/SettingsSummary.vue
+++ src/components/Completions/SettingsSummary.vue
@@ -3,7 +3,7 @@
         <wwEditorIcon name="minu-sm" class="mr-2" :class="{ 'mt-1': !!index }" />
         <div class="text-ellipsis">{{ completionsPrompt.title }}</div>
     </div>
-    <div class="flex items-center body-2" v-if="!completionsPrompt.length">
+    <div class="flex items-center body-2" v-if="!completionsPrompts.length">
         <wwEditorIcon name="minu-sm" class="mr-2" />
         <div class="text-ellipsis">No prompt defined.</div>
     </div>
